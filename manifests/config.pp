c5c13aa2c4b723c47656785f97073e776523deaa
Ash Crosby <tcrosby@gmail.com>
2021-04-30 17:55:35 +0100

    SCREM

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,11 +5,8 @@ class superset::config inherits superset {
 
   $secret_key = seeded_rand_string(64, "${::fqdn}::superset_secret", 'abcdef0123456789')
 
-  $filter_string = (ldap_roles_mapping.map |$group, $role| { "(memberOf=cn=${group},${ldap_group_dn})" }).join()
-  notify { $filter_string: }
-
   $ldap_filter_string = $ldap_user_filter? {
-    undef => "(&(objectClass=person)(|${filter_string}))",
+    undef => "(&(objectClass=person)(|${($ldap_roles_mapping.map |$group, $role| { "(memberOf=cn=${group},${ldap_group_dn})" }).join()}))",
     default => $ldap_user_filter
   }
 
