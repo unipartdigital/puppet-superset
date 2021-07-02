4ba464055ff87e7a15b56ced8411ccdf52a6cbc2
Ash Crosby <tcrosby@gmail.com>
2021-07-02 15:14:21 +0100

    remove refs to obsolete systemd calls

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,15 +1,14 @@
 # =Class superset::service
 class superset::service inherits superset {
   require superset::install
-  require systemd::systemctl::daemon_reload
 
   service { 'celery':
-    ensure  => running,
-    enable  => true,
+    ensure => running,
+    enable => true,
   }
 
   service { 'gunicorn':
-    ensure  => running,
-    enable  => true,
+    ensure => running,
+    enable => true,
   }
 }
