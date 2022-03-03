8d61cd86f5f1ad68834c3eccc0d50858f9011e7d
Étienne Boisseau-Sierra <etienne.boisseau-sierra@unipart.io>
2022-03-03 14:44:31 +0000

    feat: Enable definition of custom time grains
    Passing the following hiera param
    
    ```yaml
    superset::time_grain_addons: {
      postgresql: [
        {
          id: "PT15M",
          name: "15 minutes",
          definition: "date_trunc('hour', {col}) + date_part('minute', {col})::int / 15 * interval '15 min'",
        },
        {
          id: "PT6H",
          name: "6 hours",
          definition: "date_trunc('day', {col}) + date_trunc('hour', {col})::int / 6 * interval '6 h'",
        },
      ]
    }
    ```
    
    would result in the following addition in `config.py`:
    
    ```python
    TIME_GRAIN_ADDON_EXPRESSIONS = {
        'postgresql': {
            'PT6H': "date_trunc('day', {col}) + date_trunc('hour', {col})::int / 6 * interval '6 h'",
            'PT15M': "date_trunc('hour', {col}) + date_part('minute', {col})::int / 15 * interval '15 min'",
        },
    }
    
    TIME_GRAIN_ADDONS = {
        "PT6H": "6 hours",
        "PT15M": "15 minutes",
    }
    ```
    
    Signed-off-by: Étienne Boisseau-Sierra <etienne.boisseau-sierra@unipart.io>

--- manifests/init.pp
+++ manifests/init.pp
@@ -41,6 +41,7 @@ class superset (
   Optional[String] $logo_path = undef,
   Optional[String] $ldap_user_filter = undef,
   Optional[Hash[String[1], String[1]]] $jinja_context_addons = undef,
+  Optional[Hash[String[1], Array[Hash[String[1], String[1]]]]] $time_grain_addons = undef,
 ) {
   contain superset::db
   contain superset::package
